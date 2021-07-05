2c92aa0f7c9cdca3444a4f223e3ab52f9fec18cd
Max <max.lin@screeningeagle.com>
2021-07-05 17:27:01 +0800

    Add fighters validation

--- controller/combat.go
+++ controller/combat.go
@@ -9,11 +9,16 @@ import (
 
 func CombatController(c *gin.Context) {
 	fighters := c.QueryArray("fighters[]")
+	if len(fighters) < 2 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No. of fighters is less than 2"})
+		return
+	}
 
 	combatResult, err := combat.New(fighters[0], fighters[1])
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
 	}
 
 	c.JSON(http.StatusOK, combatResult)
